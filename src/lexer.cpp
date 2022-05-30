c0499b43cbe4df3dcb4eadc4becf4c3dba93a1b9
Satin Wuker <74630829+SatinWuker@users.noreply.github.com>
2022-05-29 20:28:46 -0700

    Update lexer.cpp

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,48 +1,69 @@
 #include<iostream>
 #include<vector>
+#include<string>
+#include<algorithm>
+#include<array>
+#include<ctype.h>
+#include "wonderwall.hpp"
 
-using namespace std;
 
-/*
-store msg 'Hello world!'
-print msg
-*/
+class Token {   // a type for storing tokens. Token(token, type)
+    public:
+        std::string token, type;
+        Token(std::string tok = " ", std::string typ = TT_MPT)
+        : token(tok), type(typ) {}
+};
 
-// void print_node(vector<string> *Node, vector<string> args);
-// void assign_node(vector<string> *Node, name, vector<string> expr);
-// void if_node(vector<string> *Node, vector<string> args);
-// void while_node(vector<string> *Node, vector<string> args);
 
-vector<string> parse(string text){
-    vector<string> ast;     // return: abstract syntax tree
+bool is_int(std::string &str) {
+    return str.find_first_not_of("0123456789") == std::string::npos;
+}
+bool is_float(std::string &str) {
+    return str.find_first_not_of("0123456789.") == std::string::npos;
+}
+
+std::string get_type(std::string &str) {
+    if (str == "\n")
+        return TT_EOF;
+
+    else if (std::find(KEYWORDS, KEYWORDS + 4, str) != KEYWORDS + 4)
+        return TT_KW;
+
+    else if (str[0] == '\'' && str.back() == '\'')
+        return TT_STR;
+
+    else if (is_int(str))
+        return TT_INT;
+
+    else if (is_float(str))
+        return TT_FLOAT;
+
+    else
+        return TT_ID;
+
+}
+
+std::vector<Token> tokenize(std::string text){
+    std::vector<Token> tokens;  // return: tokens
 
     int i = 0;              // index of the text string
     int quote_count = 0;    // count of the char '
 
-    string lastchar;        // variable to store the last token
-
+    std::string lastchar;   // variable to store the last token
     while (i < text.length()) {
         if (text[i] == '\'') quote_count++;
 
-        if (text[i] == ' ' && quote_count % 2 == 0)
+        if ((text[i] == ' '||text[i] == '\n') && quote_count % 2 == 0)
         {
-            ast.push_back(lastchar);
-            lastchar = "";
+            tokens.push_back(Token(lastchar, get_type(lastchar)));
+            lastchar.clear();
+
+            if(text[i] == '\n') tokens.push_back(Token("\n", TT_EOF));
         }
         else lastchar += text[i];
-
         i++;
     }
 
-    return ast;
-    
-}
-
-int main(int argc, char **argv){
-    string text = "store msg 'Hello world!'\nprint msg";
-    vector<string> ast = parse(text);
-    for(int i = 0; i < ast.size(); i++){
-        cout<<ast[i]<<'\n';
-    }
+    return tokens;
 
 }
