ee53ca967d3f67c69e4d31342c159e23f9dee84a
Satin Wuker <74630829+SatinWuker@users.noreply.github.com>
2022-05-29 19:45:46 -0700

    Add files via upload

---
+++ src/wonderwall.hpp
@@ -0,0 +1,10 @@
+
+#define TT_EOF    "EOF"    // End of file
+#define TT_MPT    "MPT"    // Empty
+#define TT_KW     "KW"     // Keyword
+#define TT_ID     "ID"     // Identifier
+#define TT_INT    "INT"    // Constant int
+#define TT_STR    "STR"    // Constant int
+#define TT_OP     "OP"     // Operator
+
+const std::string KEYWORDS[4] = {"print", "store", "if", "else"};
