8be7f46f9cc08b35a33c40a490064b00c4e2ef79
Satin Wuker <74630829+SatinWuker@users.noreply.github.com>
2022-05-31 12:37:08 -0700

    Update wonderwall.hpp

--- src/wonderwall.hpp
+++ src/wonderwall.hpp
@@ -4,7 +4,8 @@
 #define TT_KW     "KW"     // Keyword
 #define TT_ID     "ID"     // Identifier
 #define TT_INT    "INT"    // Constant int
-#define TT_STR    "STR"    // Constant int
+#define TT_FLOAT  "FLOAT"  // Constant float
+#define TT_STR    "STR"    // Constant string
 #define TT_OP     "OP"     // Operator
 
 const std::string KEYWORDS[4] = {"print", "store", "if", "else"};
