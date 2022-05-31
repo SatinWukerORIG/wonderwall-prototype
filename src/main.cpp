fb25347fa24f49fab29f256fb4449bc4078e395e
Satin Wuker <74630829+SatinWuker@users.noreply.github.com>
2022-05-31 12:36:24 -0700

    Read from file

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,26 @@
+#include <fstream>
 #include "lexer.cpp"
 #include "parser.cpp"
 
-int main(int argc, char **argv){
-    std::string text = "store msg 'Hello world!'\nprint msg";
-    text += " ";
-    auto tokens = tokenize(text);
+std::string readFile(const char* filename) {
+    std::string content, line;
+    std::ifstream file(filename);
 
-    for(int i = 0; i < tokens.size(); i++){
-        std::cout<<tokens[i].token<<" : "<<tokens[i].type<<"\n";
+    while (getline(file, line)) {
+        content = content + line + "\n";
     }
 
-    std::vector<ExprAST> AST;
-    Parser parser(tokens, AST);
+    return content;
+}
+
+
+int main(int argc, char *argv[]){
+    std::string text = readFile(argv[1]);   // code from the target file
+
+    auto tokens = lexer::tokenize(text);
+
+    std::vector<parser::ExprAST> AST;
+    parser::Parser parser(tokens, AST);
+    parser.parse();
 
 }
