8bd20a3005ab3cee5154977d8c1d005ab0d0e04a
Satin Wuker <74630829+SatinWuker@users.noreply.github.com>
2022-06-27 15:11:49 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,44 @@
 #include<iostream>
 #include "lexer.cpp"
 #include "parser.cpp"
+#include "intpr.cpp"
 
-std::string readFile(const char* filename) {
-    std::string content, line;
+std::vector<std::vector<lexer::Token>> readFile(const char* filename) {
+    std::string line;
+    std::vector<std::vector<lexer::Token>> tokens;
     std::ifstream file(filename);
 
-    while (getline(file, line))
-        content = content + line + "\n";
+    while (getline(file, line)) {
+        line += "\n";
+        tokens.push_back(lexer::tokenize(line));
+    }
+    file.close();
 
-    return content;
+    return tokens;
+
+}
+
+void showlist(std::vector<lexer::Token> &vect){
+    std::cout<<"[";
+    for(auto it: vect){
+        if (it.token == "\n")
+            std::cout<<"'\\n'";
+        else
+            std::cout<<it.token<<", ";
+    }
+    std::cout<<"]\n";
 }
 
 int main(int argc, char *argv[]){
-    std::string text = readFile(argv[1]);   // code from the target file
+    auto tokens = readFile(argv[1]);   // code from the target file
 
-    lexer::Lexer lexer(text);
+    for(auto it1: tokens){
+        showlist(it1);
+    }
 
-    std::vector<parser::ExprAST> AST;
-    parser::Parser parser(lexer, AST);
-    parser.parse();
+    std::vector<parser::BaseNode*> AST;
+    parser::Parser parser(tokens, AST);
+
+    intpr::interpret(AST);
 
 }
