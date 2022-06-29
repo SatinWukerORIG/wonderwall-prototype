dcb2d19db429acca2cad9d60cae7cb4121bfa344
Satin Wuker <74630829+SatinWuker@users.noreply.github.com>
2022-06-28 23:22:13 -0700

    Update intpr.cpp

--- src/intpr.cpp
+++ src/intpr.cpp
@@ -1,14 +1,29 @@
 namespace intpr {
 
-    void interpret(std::vector<parser::BaseNode*> &nodes) {
+    void showlist(std::vector<lexer::Token> &vect){
+        std::cout<<"[";
+        for(auto it: vect){
+            if (it.token == "\n")
+                std::cout<<"'\\n'";
+            else
+                std::cout<<it.token<<", ";
+        }
+        std::cout<<"]\n";
+    }
 
+    void interpret(std::vector<parser::BaseNode*> &nodes) {
+        // std::cout<<"node length: "<<nodes.size()<<std::endl;
         for(auto node: nodes) {
             if(auto it = dynamic_cast<parser::PrintNode*>(node)) {
-                std::cout<<it->name<<"\n";
+                std::cout<<"PRINT ";
+                showlist(it->expr);
+
                 delete it;
             }
             else if(auto it = dynamic_cast<parser::StoreNode*>(node)) {
-                std::cout<<it->name<<"\n";
+                std::cout<<"STORE "<<it->var_name<<" ";
+                showlist(it->expr);
+
                 delete it;
             }
         }
