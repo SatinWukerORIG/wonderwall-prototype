f2f307588b718def77dc6fcfce555e434c987b56
Satin Wuker <74630829+SatinWuker@users.noreply.github.com>
2022-07-01 21:10:39 -0700

    optimization

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,36 +1,35 @@
-#include<iostream>  // std::cout<<
-#include<vector>    // std::vector<>
-#include<string>    // std::string
-#include<algorithm> // std::find()
+#include<iostream>         // std::cout<<
+#include<vector>           // std::vector<>
+#include<algorithm>        // std::find()
 #include "wonderwall.hpp"  // TT(token types), KEYWORDS[4]
 
 
 namespace lexer {
 
-    class Token {   // a type for storing tokens. Token(token, type)
+    class Token {   // Token(token, type)
         public:
             std::string token, type;
-            Token(std::string tok = " ", std::string typ = TT_MPT)
+            Token(std::string tok = "", std::string typ = TT_MPT)
             : token(tok), type(typ) {}
     };
 
 
-    bool is_int(const std::string &str) {
+    inline bool is_int(const std::string &str) {
         std::string::const_iterator it = str.begin();
         while (it != str.end() && std::isdigit(*it)) ++it;
         return !str.empty() && it == str.end();
     }
-    bool is_float(const std::string &str) {
+    inline bool is_float(const std::string &str) {
         std::string::const_iterator it = str.begin();
         while (it != str.end() && (std::isdigit(*it) || *it == '.')) ++it;
         return !str.empty() && it == str.end();
     }
-    bool is_op(std::string &str) {
+    inline bool is_op(std::string &str) {
         return std::find(OPERATORS, OPERATORS + 5, str) != OPERATORS + 5;
     }
 
-    std::string get_type(std::string &str) {
-        if (std::find(KEYWORDS, KEYWORDS + 5, str) != KEYWORDS + 5)
+    inline std::string get_type(std::string &str) {
+        if (std::find(KEYWORDS, KEYWORDS + 6, str) != KEYWORDS + 6)
             return TT_KW;
 
         else if (str[0] == '\'' && str.back() == '\'')
@@ -44,23 +43,21 @@ namespace lexer {
     }
 
     std::vector<Token> tokenize(std::string &text){
-        std::vector<Token> tokens;  // return: tokens
-        int quote_count;            // count of the char '
+        std::vector<Token> tokens;                  // return: tokens
+        int quote_count;                            // count of the char '
 
-        std::string lastchar;       // variable to store the last token
+        std::string lastchar;                       // variable to store the last token
         for (int i = 0; i < text.length(); i++) {
             if (text[i] == '#') break;
             if (text[i] == '\'') quote_count++;
 
             if ((text[i] == ' '||text[i] == '\n') && quote_count % 2 == 0)
             {
-                //if (lastchar != " ")
-                    tokens.push_back(Token(lastchar, get_type(lastchar)));
-
+                tokens.push_back(Token(lastchar, get_type(lastchar)));
                 lastchar.clear();
 
-                if(text[i] == '\n')
-                    tokens.push_back(Token("\n", TT_EOF));
+                // if(text[i] == '\n')
+                //     tokens.push_back(Token("\n", TT_EOF));
             }
             else lastchar += text[i];
         }
