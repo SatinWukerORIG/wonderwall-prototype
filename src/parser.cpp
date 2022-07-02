9cb204b6cd7753ac202194f3df93232477a11eff
Satin Wuker <74630829+SatinWuker@users.noreply.github.com>
2022-07-01 21:07:42 -0700

    Update parser.cpp

--- src/parser.cpp
+++ src/parser.cpp
@@ -3,6 +3,7 @@ namespace parser {
     class BaseNode{
         public:
             virtual ~BaseNode() {};
+
     };
 
     class PrintNode : public BaseNode{
@@ -26,6 +27,36 @@ namespace parser {
             int idx = 0;
             std::vector<std::vector<lexer::Token>> *tokens = new std::vector<std::vector<lexer::Token>>();
 
+            inline void parse() {
+                if (tokens->at(idx)[0].token == "print") {
+                    /*  print_node
+                            |
+                            expr    */
+                    // std::cout<<"debug: print\n";
+                    AST->push_back(new PrintNode(
+                        {tokens->at(idx)[1], tokens->at(idx).back()}));
+                }
+
+                else if (tokens->at(idx)[0].token == "store") {
+                    /* store_node
+                        /    \
+                        name   expr */
+                    // std::cout<<"debug: store\n";
+                    AST->push_back(new StoreNode(tokens->at(idx)[1].token,
+                        {tokens->at(idx)[2], tokens->at(idx).back()}));
+                }
+
+                else if (tokens->at(idx)[0].token == "if") {
+                    /*
+                            if_node
+                            /   |    \
+                        cond stmts else
+                    */
+                    
+                }
+                idx++;
+            }
+
         public:
             std::vector<BaseNode*> *AST = new std::vector<BaseNode*>();
 
@@ -34,34 +65,10 @@ namespace parser {
             : tokens(tokens), AST(AST)
             {
                 while (idx < tokens->size()) {
-                    if (tokens->at(idx)[0].token == "print") {
-                        /*  print_node
-                                |
-                               expr    */
-                        // std::cout<<"debug: print\n";
-                        AST->push_back(new PrintNode(
-                            {tokens->at(idx)[1], tokens->at(idx).back()}));
-                    }
-
-                    else if (tokens->at(idx)[0].token == "store") {
-                        /* store_node
-                            /    \
-                          name   expr */
-                        // std::cout<<"debug: store\n";
-                        AST->push_back(new StoreNode(tokens->at(idx)[1].token,
-                            {tokens->at(idx)[2], tokens->at(idx).back()}));
-                    }
-
-                    // else if (tokens[idx][0].token == "if") {
-                    //     /*
-                    //           if_node
-                    //         /    |    \
-                    //         cond stmts else
-                    //     */
-                    // }
-                    idx++;
+                    parse();
                 }
             }
 
     };
+
 }
