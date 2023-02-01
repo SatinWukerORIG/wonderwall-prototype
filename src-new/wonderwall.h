50b3710a9b55e76a38310895fada903e32dc1929
Satin Wuker <74630829+SatinWuker@users.noreply.github.com>
2023-01-31 21:10:21 -0800

    Adding TT token types

--- src-new/wonderwall.h
+++ src-new/wonderwall.h
@@ -1,6 +1,8 @@
+/*Included by all files. Contains all constants*/
 #pragma once
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <vector>
 
 /* operators, sorted according their ascii values
@@ -11,3 +13,18 @@ const std::vector<char> OPERATORS {
     '+', ',', '-', '.', '/', ':',
     '<', '>', '[', ']'
 };
+// TT: Token Types
+const char TT_STR = '0';
+const char TT_I8 = '1';
+const char TT_I16 = '2';
+const char TT_I32 = '3';
+const char TT_I64 = '4';
+const char TT_I128 = '5';
+const char TT_IINDEX = '6';
+const char TT_U8 = '7';
+const char TT_U16 = '8';
+const char TT_U32 = '9';
+const char TT_U64 = 'A';
+const char TT_U128 = 'B';
+const char TT_UIINDEX = 'C';
+const char TT_IDFR = 'D';
