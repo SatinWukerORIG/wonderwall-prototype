2a4c3098ee31339209287dde912997037d77d6ab
Satin Wuker <74630829+SatinWuker@users.noreply.github.com>
2023-02-01 22:15:13 -0800

    Update build.bat

--- src-new/build.bat
+++ src-new/build.bat
@@ -1,2 +1,1 @@
-clang++ -O3 -ffunction-sections -fdata-sections main.cpp -o wonderwall.exe
-wonderwall helloworld.ww
+clang++ -O3 -std=c++17 -ffunction-sections -fdata-sections main.cpp -o wonderwall.exe
