0797ed4a4a41f4c40db5b8fd9ec974e75209559b
Satin Wuker <74630829+SatinWuker@users.noreply.github.com>
2023-02-01 22:15:39 -0800

    Update executor.cpp

--- src-new/executor.cpp
+++ src-new/executor.cpp
@@ -9,10 +9,7 @@ class Token{
 public:
     char ttype;
     std::string tvalue;
-    Token(char type, std::string value): ttype(type), tvalue(value) {
-        ttype = type;
-        tvalue = value;
-    }
+    Token(char type, std::string value): ttype(type), tvalue(value) {}
 };
 
 class Executor{
@@ -24,58 +21,61 @@ std::vector<std::string> symbol_table {};
 
 void tokenize() {
     unsigned short int i = 0;
-    const unsigned int src_len = src.length();
+    const unsigned int src_len = src -> length();
     std::string current_tok;
+    current_tok.reserve(4);
     char ch;
     do {
-        ch = src[i];
+        ch = (*src)[i];
         // next line
         if (ch == '\n') lines++;
         // comment
         else if(ch == '#'){
             i++;
-            while(src[i] != '\n') i++;
+            while((*src)[i] != '\n') i++;
         }
         // Tokenize number
-        else if (ch >= '0' && ch <= '9') {
-            while(src[i] >= '0' && src[i] <= '9' && i < src_len){
-                current_tok.push_back(src[i]);
+        else if (ch > 47 && ch < 58) {
+            while((*src)[i] > 47 && (*src)[i] < 58 && i < src_len){
+                current_tok.push_back((*src)[i]);
                 i++;
             }
             Token token(TT_I16, current_tok);
             //std::cout<<"token: "<<token.ttype<<token.tvalue<<std::endl;
             current_tok.clear();
+            current_tok.shrink_to_fit();
         }
         // Tokenize string
         else if (ch == '"') {
             i++;
-            while(src[i] != '"' && i < src_len){
-                current_tok.push_back(src[i]);
+            while((*src)[i] != '"' && i < src_len){
+                current_tok.push_back((*src)[i]);
                 i++;
             }
             Token token(TT_STR, current_tok);
             //std::cout<<"token: "<<token.ttype<<token.tvalue<<std::endl;
             current_tok.clear();
+            current_tok.shrink_to_fit();
         }
         // Tokenize identifier
         else {
             // Identifier
-            while(!std::binary_search(OPERATORS.begin(), OPERATORS.end(), src[i])
+            while(!std::binary_search(OPERATORS.begin(), OPERATORS.end(), (*src)[i])
             && i < src_len){
-                current_tok.push_back(src[i]);
+                current_tok.push_back((*src)[i]);
                 i++;
             }
             Token token(TT_IDFR, current_tok);
             //std::cout<<"token: "<<token.ttype<<token.tvalue<<std::endl;
             current_tok.clear();
+            current_tok.shrink_to_fit();
         }
-        i++;
     }
     while (i++ < src_len);
 
 }
 public:
-    std::string src;
+    std::unique_ptr<std::string> src;
 
     void run() {
         tokenize();
