012ec5e522fd673a123fabd1230380868e3c0195
Satin Wuker <74630829+SatinWuker@users.noreply.github.com>
2023-02-22 21:44:50 -0800

    new parser, new ast

--- src-new/parser.cpp
+++ src-new/parser.cpp
@@ -1,22 +1,54 @@
+#include <stdio.h>
 #include <string>
 #include <vector>
 
+#include "lexer.cpp"
+// lexer.cpp is already included
+
 namespace parser {
-    class Token{
-    public:
-        char ttype;
-        std::string tvalue;
-        Token(char type, std::string value): ttype(type), tvalue(value) {}
-    };
 
-    inline std::vector<Token> make_store_tree(Token var_name, Token value){
-        // [name="store", var_name="", value=""]
-        return {Token('K', "store"), var_name, value};
+class Parser{
+private:
+    lexer::Lexer Lex;
+    void print_tok(lexer::Token tok){
+        std::cout<<"Type: "<<tok.ttype<<" Value: " <<tok.tvalue<<std::endl;
     }
 
-    inline std::vector<Token> make_print_tree(Token value){
-        // [name="print", value=""]
-        return {Token('K', "print"), value};
-    }
+    void parse_instrc(lexer::Token tk) { // tk: token
+        if (tk.tvalue == "print") {
 
+        }
+    }
+    /*
+    TODO: type check of an identifier: std::string
+          will be invoked in parse() while() switch case: TT_IDFR
+          return results: Built-in instruction(Keyword), Instruction/Function/Variable
+    */
+    char get_idfr_type(std::string& idfr) {
+        
+        return ' ';
+    }
+public:
+    Parser(lexer::Lexer Lex): Lex(Lex){}
+
+    void parse() { // start parsing. main loop
+        unsigned int idx = 0;
+        lexer::Token CurTok('W', "");
+        while (1) {
+            CurTok = Lex.get_tok();
+            if (CurTok.ttype == TT_EOF) {
+                break;
+            }
+            switch (CurTok.ttype) {
+                case TT_IDFR: // identifier can be an instruction, function, or variable
+                    parse_instrc(CurTok);
+                    std::cout<<"IDFR: "<<CurTok.tvalue<<std::endl;
+                    break;
+                default:
+                    std::cout<<CurTok.ttype<<": "<<CurTok.tvalue<<std::endl;
+                
+            }
+        }
+    }
+};
 };
